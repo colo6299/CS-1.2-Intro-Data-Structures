d2ee4efcd95a68e2c7c17a520a917ef992db46ee
= <=>
2019-12-09 01:44:40 -0800

    Add two queue types
    FixedQ is more of a window, really

---
+++ Code/krappy_markov_substitute/ngram_markov.py
@@ -0,0 +1,13 @@
+import random
+from arq import FixedQ
+from hiptr import Triptor, Diptor
+from hiptr import Histhor as Histogram
+import string
+from bessemer import aesop, clean
+import cProfile
+import re
+import sys
+
+
+# Well, fine. I'll do it right this time. 
+
